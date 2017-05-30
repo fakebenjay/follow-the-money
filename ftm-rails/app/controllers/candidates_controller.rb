e2d1ab014f8efcc9c9c2f6f847124b57aaeeac02
fakebenjay <fakebenjay@gmail.com>
2017-05-30 19:12:06 -0400

    building out Candidate scraper

--- ftm-rails/app/controllers/candidates_controller.rb
+++ ftm-rails/app/controllers/candidates_controller.rb
@@ -1,2 +1,23 @@
+require 'open-uri'
+
 class CandidatesController < ApplicationController
+  def create
+    candidate = Candidate.new
+    url = 'https://www.opensecrets.org/politicians/summary.php?cid=N00001003'
+    html = open(url)
+    raw_candidate = Nokogiri::HTML(html)
+
+    byebug
+
+    candidate.name = 'Boberto I'
+    candidate.state = 5
+    candidate.district = 'At Large'
+    candidate.office = 'Emperor'
+    candidate.party = 1
+    if candidate.save
+      render json: candidate, serializer: CandidateSerializer
+    else
+      render json: "There was an issue saving this candidate", status: 401
+    end
+  end
 end
---
+++ ftm-rails/app/serializers/candidate_serializer.rb
@@ -0,0 +1,3 @@
+class CandidateSerializer < ActiveModel::Serializer
+  attributes :id, :name, :state_id, :district, :office, :party_id 
+end
